fc8b732a78a2e9e7be683efed80815d970234715
Juan Olloniego <juanolloniego@gmail.com>
2019-06-26 19:56:25 +0100

    Refactored files and encapsulated data processing utilities

--- build_vocabulary.py
+++ build_vocabulary.py
@@ -1,70 +1,26 @@
 import pickle
-import tokenize
 import itertools
 import collections
-
-TRAIN_DATA = 'data/train.txt'
-
-STR_TOKEN = '<STR>'
-NUM_TOKEN = '<NUM>'
-INDENT_TOKEN = '<IND>'
-DEDENT_TOKEN = '<DED>'
-OOV_TOKEN, OOV_IDX = '<OOV>', 0
-PAD_TOKEN, PAD_IDX = '<PAD>', 1
+import data_utils as du
 
 
-def read_data(data_path):
-    data = []
-    with open(data_path, 'r') as train_db:
-        python_files = [x for x in train_db.read().splitlines()]
-
-    # Read each code file to be used from the specified path
-    for filename in python_files:
-        try:
-            with open(filename, 'r') as current_file:
-                tokens = tokenize.generate_tokens(current_file.readline)
-
-                # Dont process comments, block comments or empty tokens
-                data.append([preprocess(t_type, t_val) for t_type, t_val, _, _, _ in tokens
-                             if t_type != tokenize.COMMENT and
-                             not t_val.startswith("'''") and
-                             not t_val.startswith('"""') and
-                            (t_type == tokenize.DEDENT or t_val != "")])
-
-        except:
-            print("Error with file:", filename)
-
-    return data
-
-
-def preprocess(token_type, token_val):
-    if token_type == tokenize.NUMBER:
-        return NUM_TOKEN
-    if token_type == tokenize.INDENT:
-        return INDENT_TOKEN
-    if token_type == tokenize.DEDENT:
-        return DEDENT_TOKEN
-    if token_type == tokenize.STRING:
-        return STR_TOKEN
-    return token_val
-
-
-def build_vocab(data):
+def build_vocab(dataset):
     oov_threshold = 0
-    counter = collections.Counter(itertools.chain(itertools.chain(*data)))
+    counter = collections.Counter(itertools.chain(itertools.chain(*dataset)))
     count_pairs = sorted(counter.items(), key=lambda x: -x[1])
 
     count_pairs = (p for p in count_pairs if p[1] > oov_threshold)
 
     words, _ = list(zip(*count_pairs))
     word_to_id = dict(zip(words, range(2, len(words) + 2)))
-    word_to_id[OOV_TOKEN] = OOV_IDX
-    word_to_id[PAD_TOKEN] = PAD_IDX
+    word_to_id[du.OOV_TOKEN] = du.OOV_IDX
+    word_to_id[du.PAD_TOKEN] = du.PAD_IDX
     return word_to_id
 
 
 if __name__ == "__main__":
-    data = read_data('data/train.txt')
+    data_for_vocab = 'data/train.txt'
+    data = du.read_data(data_for_vocab)
     print("Loaded {} files".format(len(data)))
     vocab = build_vocab(data)
     print("Vocabulary size is:", len(vocab))
---
+++ data_utils.py
@@ -0,0 +1,44 @@
+import tokenize
+
+STR_TOKEN = '<STR>'
+NUM_TOKEN = '<NUM>'
+INDENT_TOKEN = '<IND>'
+DEDENT_TOKEN = '<DED>'
+OOV_TOKEN, OOV_IDX = '<OOV>', 0
+PAD_TOKEN, PAD_IDX = '<PAD>', 1
+
+
+def read_data(data_path):
+    data = []
+    with open(data_path, 'r') as train_db:
+        python_files = [x for x in train_db.read().splitlines()]
+
+    # Read each code file to be used from the specified path
+    for filename in python_files:
+        try:
+            with open(filename, 'r') as current_file:
+                tokens = tokenize.generate_tokens(current_file.readline)
+
+                # Dont process comments, block comments or empty tokens
+                data.append([preprocess(t_type, t_val) for t_type, t_val, _, _, _ in tokens
+                             if t_type != tokenize.COMMENT and
+                             not t_val.startswith("'''") and
+                             not t_val.startswith('"""') and
+                            (t_type == tokenize.DEDENT or t_val != "")])
+
+        except:
+            print("Error with file:", filename)
+
+    return data
+
+
+def preprocess(token_type, token_val):
+    if token_type == tokenize.NUMBER:
+        return NUM_TOKEN
+    if token_type == tokenize.INDENT:
+        return INDENT_TOKEN
+    if token_type == tokenize.DEDENT:
+        return DEDENT_TOKEN
+    if token_type == tokenize.STRING:
+        return STR_TOKEN
+    return token_val

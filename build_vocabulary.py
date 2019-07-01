3a36fd088eb09042246ce5dc581bfde219689327
Juan Olloniego <juanolloniego@gmail.com>
2019-07-01 19:07:32 +0100

    Add one more argument and default values for the existing ones

--- build_vocabulary.py
+++ build_vocabulary.py
@@ -7,7 +7,8 @@ import data_utils as du
 # Argument parsing
 parser = argparse.ArgumentParser()
 parser.add_argument('--vocab_data', type=str, help='Path to file containing paths to the data to use.')
-parser.add_argument('--data_root', type=str, help='Path root folder containing the cloned repositories.')
+parser.add_argument('--data_root', default='data', type=str, help='Path root folder containing the cloned repositories.')
+parser.add_argument('--out_path', default='data', type=str, help='Path to save vocabulary object.')
 args = parser.parse_args()
 
 
@@ -32,4 +33,5 @@ if __name__ == "__main__":
     print("Loaded {} files".format(len(data)))
     vocab = build_vocab(data)
     print("Vocabulary size is:", len(vocab))
-    pickle.dump(vocab, open("data/vocab.p", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+    out_file = args.out_path + "/vocab.p"
+    pickle.dump(vocab, open(out_file, "wb"), protocol=pickle.HIGHEST_PROTOCOL)

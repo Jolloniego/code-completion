6d663b5ac14c204ee06baee98f32c12d3196b0e4
Juan Olloniego <juanolloniego@gmail.com>
2019-07-03 11:27:58 +0100

    Added possibiity to limit the vocabulary siz

--- build_vocabulary.py
+++ build_vocabulary.py
@@ -6,14 +6,16 @@ import data_utils as du
 
 # Argument parsing
 parser = argparse.ArgumentParser()
-parser.add_argument('--vocab_data', type=str, help='Path to file containing paths to the data to use.')
-parser.add_argument('--data_root', default='data', type=str, help='Path root folder containing the cloned repositories.')
+parser.add_argument('--vocab_data', default='data/train.txt',
+                    type=str, help='Path to file containing paths to the data to use.')
+parser.add_argument('--data_root', default='data/repos', type=str,
+                    help='Path root folder containing the cloned repositories.')
 parser.add_argument('--out_path', default='data', type=str, help='Path to save vocabulary object.')
+parser.add_argument('--oov_threshold', default=20, type=int, help='Ignore words that appear less than this many times.')
 args = parser.parse_args()
 
 
-def build_vocab(dataset):
-    oov_threshold = 0
+def build_vocab(dataset, oov_threshold):
     counter = collections.Counter(itertools.chain(itertools.chain(*dataset)))
     count_pairs = sorted(counter.items(), key=lambda x: -x[1])
 
@@ -31,7 +33,7 @@ if __name__ == "__main__":
     data_root = args.data_root
     data = du.read_data(data_for_vocab, data_root)
     print("Loaded {} files".format(len(data)))
-    vocab = build_vocab(data)
+    vocab = build_vocab(data, args.oov_threshold)
     print("Vocabulary size is:", len(vocab))
     out_file = args.out_path + "/vocab.p"
     pickle.dump(vocab, open(out_file, "wb"), protocol=pickle.HIGHEST_PROTOCOL)

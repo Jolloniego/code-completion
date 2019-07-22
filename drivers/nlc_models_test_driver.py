Fixing accuracy computation and removed loss from tests

drivers/nlc_models_test_driver.py
@@ -68,13 +68,13 @@ def next_line_prediction_test(model, word_to_idx, device, model_name, args):
             preds, hidden = model(x, hidden)
 
             # Track accuracy
-            total += len(x)
+            total += 1
             preds = torch.argmax(nn.functional.softmax(preds, dim=1), dim=1).detach()
             correct += 1 if torch.equal(preds, y) else 0
 
         # Advance to the next batch
         sample, file_changed = test_dataset_batcher.get_batch()
 
-    print("Next-Line Test Set | Accuracy {:.2f} % | Loss {:.2f} | Time taken {:.2f} seconds"
-          .format(correct / total * 100, total_loss / total, time.time() - start))
+    print("Next-Line Test Set | Accuracy {:.2f} % | Time taken {:.2f} seconds"
+          .format(correct / total * 100, time.time() - start))
 

drivers/nlc_train_driver.py
@@ -48,9 +48,9 @@ def train(model, word_to_idx, device, args):
                 loss = criterion(preds, y)
 
                 # Track accuracy as well
-                total += len(x)
+                total += 1
                 preds = torch.argmax(nn.functional.softmax(preds, dim=1), dim=1).detach()
-                correct += (preds == y).sum().item()
+                correct += 1 if torch.equal(preds, y) else 0
 
                 # Backprop the loss and update params, use gradient clipping if specified
                 loss.backward(retain_graph=True)
@@ -102,9 +102,9 @@ def validate(model, val_dataset, start_time, device, args):
             validation_loss += loss / len(x)
 
             # Track accuracy
-            total += len(x)
+            total += 1
             preds = torch.argmax(nn.functional.softmax(preds, dim=1), dim=1).detach()
-            correct += (preds == y).sum().item()
+            correct += 1 if torch.equal(preds, y) else 0
 
         # Advance to the next batch
         sample, file_changed = val_dataset_batcher.get_batch()

drivers/nt_models_test_driver.py
@@ -53,7 +53,6 @@ def next_line_prediction_test(model, word_to_idx, device, model_name, args):
 
     correct = 0
     total = 0
-    total_loss = 0
     sample, file_changed = test_dataset_batcher.get_batch()
     while sample is not None:
         for idx in range(len(sample[0])):
@@ -75,15 +74,12 @@ def next_line_prediction_test(model, word_to_idx, device, model_name, args):
                 predictions, hidden = model(predicted_word.unsqueeze(0), hidden)
 
             final_output = torch.cat(final_output)
-            loss = torch.sum(final_output - y).item() / args.seq_length
-
             total += 1
-            correct += 1 if loss == 0 else 0
-            total_loss += loss
+            correct += 1 if torch.equal(final_output, y) else 0
 
         # Advance to the next batch
         sample, file_changed = test_dataset_batcher.get_batch()
 
-    print("Next-Line Test Set | Accuracy {:.2f} % | Loss {:.2f} | Time taken {:.2f} seconds"
-          .format(correct / total * 100, total_loss / total, time.time() - start))
+    print("Next-Line Test Set | Accuracy {:.2f} % | Time taken {:.2f} seconds"
+          .format(correct / total * 100, time.time() - start))
 